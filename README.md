f04874d10ee8a42a845672cef9678fb0645a76a1
Giancarlo Garcia <ggarciadeleon@csustan.edu>
2020-12-07 16:11:39 -0800

    First Commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Android Studio TextWatcher Implementation
+
+---
+
+TextWatcher is an interface that can be used to have editable actionables on text.
+
+This implementation uses three public methods:
+```
+    afterTextChanged(Editable s)
+    beforeTextChanged(CharSequence s, int start, int count, int after)
+    onTextChanged(CharSequence s, int start, int before, int count)
+```
+The application takes in input through an Edit Text and sets every new Character Change inside of a TextView. There are logs indicating the progression of the TextWatcher. Notably, all logs indicate that all methods occur in the sucession of:
+```
+    beforeTextChanged()
+    onTextChanged()
+    afterTextChanged()
+```
+No depencies needed to be installed. The interface was added in API level 1.
+
+Example of the implementation:
+![Implementation of TextWatcher](https://i.imgur.com/DadgEQl.png)
+
+References:
+[Google Documentation on TextWatcher](https://developer.android.com/reference/android/text/TextWatcher#afterTextChanged(android.text.Editable)
+
+
+---
+Android Studio Canary Details
+    SdkVersion 30
+    
+Author: @ggiande
+
---
+++ app/src/main/java/com/example/textwatcherexample/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.textwatcherexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    public EditText etInput;
+    public TextView tvOutput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etInput = findViewById(R.id.etMain);
+        etInput.addTextChangedListener(filterTextWatcher);
+        tvOutput = findViewById(R.id.tvOutput);
+    }
+
+    TextWatcher filterTextWatcher = new TextWatcher() {
+        public void afterTextChanged(Editable s) {
+            String text = etInput.getText().toString();
+            if (text != null) {
+                tvOutput.setText(text);
+            } else {
+                tvOutput.setText(R.string.EmptyTextInput);
+            }
+            Log.i(TAG, "Text has changed: Called afterTextChanged");
+        }
+
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            Log.i(TAG, "Text has changed: Called beforeTextChanged");
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            Log.i(TAG, "Text has changed: Called onTextChanged");
+        }
+    };
+}
